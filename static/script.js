c276ad7c3bd4e827692ff79ff0877b051cc1b91f
Clemens-Helling <167019903+Clemens-Helling@users.noreply.github.com>
2025-09-19 18:03:45 +0200

    Update API_BASE_URL to remove trailing slash
    Removed trailing slash from API_BASE_URL.

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 // Konfiguration
-const API_BASE_URL = 'https://smv-antrage.onrender.com/'; // Passe diese URL bei Bedarf an
+const API_BASE_URL = 'https://smv-antrage.onrender.com'; // Passe diese URL bei Bedarf an
 
 // DOM-Elemente
 const views = document.querySelectorAll('.view');
